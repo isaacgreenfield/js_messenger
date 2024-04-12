641a53a008995b34528d49d49413cd1cf5426d52
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-12 22:03:27 +0300

    Create script.js

---
+++ client_modules/menu/script.js
@@ -0,0 +1,14 @@
+const menuPopup = new Popup({
+    closable: true,
+    className: 'menu',
+    html: templateManager.createHTML('menu'),
+    removeOnClose: false,
+    windowAnimation: {
+        open: 'menu-appear 300ms ease-in-out forwards',
+        close: 'menu-disappear 300ms ease-in-out forwards'
+    }
+})
+
+function callMenu() {
+    menuPopup.open()
+}
