7cec1a5289ff39aa689bc9d31470317f317f2c02
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 16:22:11 +0300

    Create form.js

---
+++ views/js/form.js
@@ -0,0 +1,15 @@
+function loadForm(onCreate) {
+    let formName = document.querySelector('form').getAttribute('name');
+    let mainForm = new Form(formName, null, (form, r) => {
+        document.querySelector('.page-header').textContent = r.title;
+
+        if (onCreate)
+            onCreate(form, r);
+
+        form.addSubmitButton('Отправить', (res) => {
+            let params = new URLSearchParams(window.location.search);
+            location.href = params.get('next') || '/';
+        })
+
+    }, () => { alert('error!') });
+}
