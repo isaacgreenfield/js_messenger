5dbeb0793541ad929cf01690e078ad55508f38cd
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 16:26:42 +0300

    Create user.js

---
+++ views/js/user.js
@@ -0,0 +1,107 @@
+let url = new URLSearchParams(window.location.search)
+let fsButton = document.querySelector('.button.add-friend')
+let transactionButton = document.querySelector('.button.ebank')
+let user = null
+
+function togglefsbutton(state) {
+    fsButton.style.pointerEvents = state? 'all' : 'none'
+}
+
+function changedFriendshipStatus(promise, fetch=true) {
+    togglefsbutton(false)
+
+    return promise.then(r => {
+        return fetch? r.json() : r
+    })
+    .then((r) => {
+        if(!r.success)
+            alert('Ошибка!')
+        return updateFriendshipStatus()
+    })
+}
+
+function updateFriendshipButton(status) {
+    togglefsbutton(true)
+
+    if(status.friends) {
+        fsButton.textContent = 'Удалить из друзей'
+        fsButton.onclick = () => changedFriendshipStatus(
+            fetch('/remove_friend?id=' + user.id))
+    } else if(status.outgoing_request) {
+        fsButton.textContent = 'Отозвать заявку'
+        fsButton.onclick = () => changedFriendshipStatus(
+            fetch('/cancel_friend_request?id=' + user.id))
+    } else if (status.incoming_request) {
+        fsButton.textContent = 'Принять заявку'
+        fsButton.onclick = () => changedFriendshipStatus(
+            fetch('/answer_friend_request?id=' + user.id))
+    } else {
+        fsButton.textContent = 'Добавить в друзья'
+        fsButton.onclick = () => changedFriendshipStatus(
+            fetch('/send_friend_request?id=' + user.id))
+    }
+}
+
+function updateFriendshipStatus() {
+    return fetch('/getfriendshipstatus?id=' + user.id)
+    .then(r => r.json())
+    .then(r => {
+        updateFriendshipButton(r)
+    })
+}
+
+function loadLastSeen() {
+    let lastSeenLabel = document.querySelector('.user .last-seen')
+    lastSeenLabel.textContent = utils.getLastSeenStatus(
+        user.last_seen? new Date(user.last_seen) : null)
+}
+
+function loadFeed() {
+    fetch('/auth')
+    .then(r => r.json())
+    .then(r => {
+        let feed = new Feed(r, user.id,
+            document.querySelector('.feed'),
+            document.querySelector('main'))
+    })
+}
+
+function setupTransactionButton() {
+    transactionButton.href = '/ebank?sendto=' + user.tag
+}
+
+function loadFriends() {
+    let friendlistButton = document.querySelector('.info .friends')
+
+    fetch('/getfriendsofuser?id=' + user.id)
+    .then(r => r.json())
+    .then(friends => {
+
+        friendlistButton.textContent = friends.length?
+            utils.nItemsLabel(friends.length, 'друг', 'друга', 'друзей') : 'нет друзей'
+
+        console.log(friends)
+
+        let popup = new Popup({
+            html: templateManager.createHTML('friends', {friends: friends}),
+            closable: true,
+            removeOnClose: false
+        })
+
+        friendlistButton.addEventListener('click', () => {
+            popup.open()
+        })
+
+    })
+}
+
+
+utils.getUser(url.get('id'), url.get('tag'))
+.then(user_ => {
+    user = user_
+    updateFriendshipStatus()
+    loadLastSeen()
+    loadFeed()
+    setupTransactionButton()
+    loadFriends()
+})
