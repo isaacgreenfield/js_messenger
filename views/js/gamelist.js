49d7c7dfc4bb5d8c0f8ea9040f3ce1b69fe3f355
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 16:23:33 +0300

    Create gamelist.js

---
+++ views/js/gamelist.js
@@ -0,0 +1,5 @@
+document.querySelectorAll('.game-tile').forEach(game => {
+    game.addEventListener('click', () => {
+        window.location.replace('/game?id=' + game.getAttribute('gameid'))
+    })
+})
