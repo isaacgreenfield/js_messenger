9cd95036da6de1d93ec8ccb075b06007dd1566ce
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 16:21:18 +0300

    Create create_post.js

---
+++ views/js/create_post.js
@@ -0,0 +1,19 @@
+loadForm((form) => {
+    let unit = templateManager.createElement('post-body-editor')
+
+    let textarea = unit.querySelector('textarea')
+
+    unit.querySelector('.preview').addEventListener('click', () => {
+        let popup = new Popup({
+            html: templateManager.createHTML('post-body-preview', {body: textarea.value}),
+            closable: true,
+            className: 'post-body-preview'
+        })
+
+        popup.open()
+    })
+
+    form.setupCustomUnit('text', unit, (fd) => {
+        fd.append('text', textarea.value)
+    })
+})
