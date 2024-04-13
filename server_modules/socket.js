f3e1d28172540008dc23a5435c0deff3c52443e4
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 15:46:52 +0300

    Create socket.js

---
+++ server_modules/socket.js
@@ -0,0 +1,50 @@
+class Socket {
+    constructor(cfx) {
+        this.cfx = cfx
+        this.io = cfx.core.io
+        this.listeners = []
+
+        this.io.on('connection', socket => {
+            let userid = socket.request.session.userid
+
+            if (userid)
+                socket.join('u:' + userid)
+
+            this.listeners.forEach(l => l(socket, userid))
+        })
+
+    }
+
+    onSocket(f) {
+        this.listeners.push(f)
+    }
+
+    getSocketsInRoom(room) {
+        return this.io.in(room).fetchSockets()
+    }
+
+    getSocketByUserId(userid) {
+        return this.getSocketsInRoom('u:' + userid)
+        .then(sockets => {
+            return sockets.length? sockets[0] : null
+        })
+    }
+
+    forEverySocketInRoom(room, f) {
+        this.getSocketsInRoom(room)
+        .then((sockets) => {
+            sockets.forEach(socket => f(socket))
+        })
+    }
+
+    disconnectWithError(socket, err) {
+        socket.emit('error', err)
+        socket.disconnect(0)
+    }
+
+}
+
+
+exports.init = (cfx) => {
+    cfx.socket = new Socket(cfx)
+}
