0cdd3a293ad446e333e4379625edfdee8ae58693
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2024-04-13 16:22:36 +0300

    Create friends.js

---
+++ views/js/friends.js
@@ -0,0 +1,45 @@
+document.querySelector('.add-friend').addEventListener('click', () => {
+
+    let popup = new Popup({
+        title: 'Добавить друга',
+        closable: true,
+        html: '<form name="addfriend"></form>',
+    })
+
+    popup.on('hidden', () => location.reload())
+
+    let form = new Form('addfriend', null)
+
+    popup.addOption('Отмена', () => true)
+    popup.addOption('Добавить', () => {
+        if (form)
+            form.submit(() => {
+                popup.close()
+            })
+    })
+
+    popup.open()
+})
+
+function answerRequest(id, accept) {
+    fetch(`/answer_friend_request?accept=${accept? 1 : 0}&id=${id}`)
+    .then(() => location.reload())
+}
+
+function declineRequest(id) {
+    answerRequest(id, 0)
+}
+
+function acceptRequest(id) {
+    answerRequest(id, 1)
+}
+
+function cancelRequest(id) {
+    fetch(`/cancel_friend_request?id=${id}`)
+    .then(() => location.reload())
+}
+
+function removeFriend(id) {
+    fetch(`/remove_friend?id=${id}`)
+    .then(() => location.reload())
+}
